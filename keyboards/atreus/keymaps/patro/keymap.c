ce494550d71e0c99ca0fcce9fb86c98791750aa3
Patro <code@patrickrompf.de>
2020-10-18 01:26:40 +0200

    Use new way to reference keycodes for mac os

--- keyboards/atreus/keymaps/patro/keymap.c
+++ keyboards/atreus/keymaps/patro/keymap.c
@@ -2,7 +2,6 @@
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
 #include QMK_KEYBOARD_H
-#include "keymap_german.h"
 #include "keymap_german_osx.h"
 
 enum unicode_names {
@@ -38,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
   [_QW] = LAYOUT( /* Qwertz */
-    DE_OSX_Q, DE_OSX_W, DE_OSX_E, DE_OSX_R, DE_OSX_T,                  DE_OSX_Z, DE_OSX_U, DE_OSX_I,    DE_OSX_O,   DE_OSX_P,
-    DE_OSX_A, DE_OSX_S, DE_OSX_D, DE_OSX_F, DE_OSX_G,                  DE_OSX_H, DE_OSX_J, DE_OSX_K,    DE_OSX_L,   DE_OSX_HASH,
-    DE_OSX_Y, DE_OSX_X, DE_OSX_C, DE_OSX_V, DE_OSX_B,                  DE_OSX_N, DE_OSX_M, DE_OSX_COMM, DE_OSX_DOT, DE_OSX_MINS,
-    KC_TAB,   KC_LALT,  KC_LGUI,  KC_LSFT,  KC_ESC,   KC_LCTL, KC_ENT, KC_SPC,   MO(_M1),  MO(_M2),     KC_RALT,    KC_BSPC
+    DE_Q,   DE_W,    DE_E,    DE_R,    DE_T,                    DE_Z,   DE_U,    DE_I,    DE_O,    DE_P,
+    DE_A,   DE_S,    DE_D,    DE_F,    DE_G,                    DE_H,   DE_J,    DE_K,    DE_L,    DE_HASH,
+    DE_Y,   DE_X,    DE_C,    DE_V,    DE_B,                    DE_N,   DE_M,    DE_COMM, DE_DOT,  DE_MINS,
+    KC_TAB, KC_LALT, KC_LGUI, KC_LSFT, KC_ESC, KC_LCTL, KC_ENT, KC_SPC, MO(_M1), MO(_M2), KC_RALT, KC_BSPC
   ),
 
   /*
@@ -56,10 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
   [_M1] = LAYOUT(
-    DE_OSX_EXLM, DE_OSX_LBRC, DE_OSX_RBRC, DE_OSX_LCBR, DE_OSX_RCBR,                   DE_OSX_AT,   DE_OSX_UE,   DE_OSX_EQL,  DE_OSX_OE,   DE_OSX_QST,
-    DE_OSX_AE,   DE_OSX_SS,   DE_OSX_LPRN, DE_OSX_RPRN, DE_OSX_PERC,                   DE_OSX_CIRC, DE_OSX_SLSH, DE_OSX_BSLS, DE_OSX_DLR,  DE_OSX_DQOT,
-    DE_OSX_GRV,  DE_OSX_PARA, DE_OSX_LESS, DE_OSX_MORE, DE_OSX_EURO,                   DE_OSX_TILD, DE_OSX_PLUS, DE_OSX_AMPR, DE_OSX_PIPE, DE_OSX_ASTR,
-    KC_PGDN,     KC_PGUP,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_DEL
+    DE_EXLM, DE_LBRC, DE_RBRC, DE_LCBR, DE_RCBR,                   DE_AT,   DE_UDIA, DE_EQL,  DE_ODIA, DE_QUES,
+    DE_ADIA, DE_SS,   DE_LPRN, DE_RPRN, DE_PERC,                   DE_LABK, DE_SLSH, DE_BSLS, DE_DLR,  DE_DQUO,
+    DE_GRV,  DE_SECT, DE_CIRC, DE_DEG,  DE_EURO,                   DE_TILD, DE_PLUS, DE_AMPR, DE_PIPE, DE_ASTR,
+    KC_PGDN, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL
   ),
 
   /*
@@ -74,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
   [_M2] = LAYOUT(
-    KC_1,    KC_2,        KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_VOLD, KC_VOLU,     KC_MUTE, KC_MPLY, KC_MNXT,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F11,
-    KC_F1,   KC_F2,       KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    TG(_M3), DE_OSX_ACUT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_MNXT,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F11,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    TG(_M3), DE_ACUT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12
   ),
 
   /*
@@ -92,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
   [_M3] = LAYOUT(
-    DE_OSX_EQL,  KC_7, KC_8,    KC_9,    DE_OSX_ASTR,                  KC_INS,     KC_HOME,    KC_PSCR,  KC_SLCK,     KC_PAUS,
-    DE_OSX_SLSH, KC_4, KC_5,    KC_6,    DE_OSX_MINS,                  KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,     KC_MS_BTN2,
-    DE_OSX_DOT,  KC_1, KC_2,    KC_3,    DE_OSX_PLUS,                  KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_MS_BTN1,
-    DE_OSX_COMM, KC_0, KC_LGUI, KC_LSFT, KC_ESC,      KC_LCTL, KC_ENT, KC_SPC,     TO(_QW),    RESET,    KC_LALT,     KC_BSPC
+    DE_EQL,  KC_7, KC_8,    KC_9,    DE_ASTR,                  KC_INS,     KC_HOME,    KC_PSCR,  KC_SLCK,     KC_PAUS,
+    DE_SLSH, KC_4, KC_5,    KC_6,    DE_MINS,                  KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,     KC_MS_BTN2,
+    DE_DOT,  KC_1, KC_2,    KC_3,    DE_PLUS,                  KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_MS_BTN1,
+    DE_COMM, KC_0, KC_LGUI, KC_LSFT, KC_ESC,  KC_LCTL, KC_ENT, KC_SPC,     TO(_QW),    RESET,    KC_LALT,     KC_BSPC
   )
 };
